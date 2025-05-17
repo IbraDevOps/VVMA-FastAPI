41877573cb547c1858771cdeaa59f9284feb5dc4
root <root@kali.cmu.edu>
2025-05-17 10:20:22 -0400

    Fixed shared user import, confirmed BFLA using user1 deleting admin

---
+++ app/models.py
@@ -0,0 +1,8 @@
+# app/models.py
+
+users = {
+    "admin": {"password": "admin123", "role": "admin"},
+    "user1": {"password": "123456", "role": "user"}
+#     "user1": {"password": "123456", "role": "user"}
+
+}
