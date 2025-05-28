71d18b8346fba01bded88a848063ac926e5acba4
root <root@kali.cmu.edu>
2025-05-28 02:13:17 -0400

    Finalized patches and documentation for public release

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from app.routes import user
 from app.routes import auth
 from app.routes import ssrf
+from app.routes import reset
 app = FastAPI()
 
 app.include_router(user.router,prefix="/api")
 app.include_router(auth.router,prefix="/api")
 app.include_router(ssrf.router,prefix="/api")
+app.include_router(reset.router,prefix="/api")
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
